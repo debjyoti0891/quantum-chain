4175d8fb077a2e446e58e8d3acea6fa8039174ed
debjyoti_tkpad <debjyoti08911@gmail.com>
2019-07-02 00:07:27 +0800

    added requirements.txt

--- tests/testgraph.py
+++
@@ -1,9 +0,0 @@
-from quantum-chain.graphtools import *
-import sys 
-
-topoGen = TopoGraphGen()
-topoGen.loadGraph(sys.argv[1])
-topoGen.genTopoGraph(int(sys.argv[2]),int(sys.argv[3]),128)
-topoGen.storeTopoGraph('./topographs/')
-
-
